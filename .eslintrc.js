412f11cd78bd7f53971642a88494c0b8241ab93a
Evelyn Hathaway <git@evelyn.dev>
2020-04-07 23:22:55 -0700

    Update: Preload fonts, better info chips, add SVG compat to icon component

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
 
 	"extends": [
 		"plugin:evelyn/default",
+		"plugin:evelyn/babel",
 	],
 
 	"overrides": [
--- src/components/ui/Icon/index.jsx
+++ src/components/ui/Icon/index.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import classNames from "classnames";
 import styles from "./styles.module.scss";
 
-export default function Icon({icon, label, className}) {
-	label = label || icon.replace(/_/g, " ");
+export default function Icon({material, svg, label, className}) {
+	label = label || material?.replace(/_/g, " ");
 	return (
-		<span className={classNames(
-			styles.Icon,
-			className,
-		)} aria-label={label}>{icon}</span>
+		<span
+			className={classNames(
+				styles.Icon,
+				material && styles.material,
+				className,
+			)}
+			aria-label={label}
+		>
+			{material || <svg viewBox="0 0 24 24"><path fill="currentColor" d={svg}/></svg>}
+		</span>
 	);
 }
