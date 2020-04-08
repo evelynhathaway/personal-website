5f19e9900452d31b14920168d9d5c5819ef67ef4
Evelyn Hathaway <git@evelyn.dev>
2020-04-08 00:42:07 -0700

    Fix: Consistently accessible icons for font and SVG

--- src/components/ui/Icon/index.jsx
+++ src/components/ui/Icon/index.jsx
@@ -11,9 +11,10 @@ export default function Icon({material, svg, label, className}) {
 				material && styles.material,
 				className,
 			)}
-			aria-label={label}
+			aria-label={material && label}
+			role={material && "img"}
 		>
-			{material || <svg viewBox="0 0 24 24"><path fill="currentColor" d={svg}/></svg>}
+			{material || <svg viewBox="0 0 24 24"><title>{label}</title><path fill="currentColor" d={svg}/></svg>}
 		</span>
 	);
 }
